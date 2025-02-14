674ae7550aae9ad67c9e153e16fd7c5acf8ce332
Olivier <oliviernadeau2003@gmail.com>
2025-02-13 21:50:49 -0500

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float vitesseDeplacement = 10.0f;
+    // Déplacement
+    private const float vitesseDeplacement = 10.0f;
+
+    // Rotation
+    float vitesseRotation = 5.0f;
+    public bool controleVerticalInverse = true;
+    public bool controleHorizontalInverse = false;
+
+    // Terrain
+    public float distanceDuSol = 2f;
 
     void Update()
     {
-        Vector3 direction = Vector3.zero;
+        // Rotation
+        GestionBoutonDroit();
 
+        if(!Cursor.visible)
+        {
+            transform.localRotation = GestionRotation();
+        }
+
+        // Déplacement
+        transform.Translate(Acceleration() * Time.deltaTime * vitesseDeplacement * Deplacement());
+
+        if (Terrain.activeTerrain)
+        {
+            transform.position = GestionTerrain();
+        }
+    }
+
+    Vector3 Deplacement()
+    {
+        Vector3 direction = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
         {
             direction += Vector3.forward;
@@ -18,7 +45,6 @@ public class PlayerController : MonoBehaviour
         {
             direction += Vector3.back;
         }
-
         if (Input.GetKey(KeyCode.A))
         {
             direction += Vector3.left;
@@ -27,7 +53,70 @@ public class PlayerController : MonoBehaviour
         {
             direction += Vector3.right;
         }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            direction += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            direction += Vector3.down;
+        }
+        return direction;
+    }
 
-        transform.Translate(Time.deltaTime * vitesseDeplacement * direction);
+    float Acceleration()
+    {
+        float acceleration = 1.0f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            acceleration = 2.0f;
+        }
+        return acceleration;
+    }
+
+    void GestionBoutonDroit()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    Quaternion GestionRotation()
+    {
+        float rotationY = Input.GetAxis("Mouse X");
+        float rotationX = Input.GetAxis("Mouse Y");
+
+        float limite = 1f;
+        rotationY = Mathf.Clamp(rotationY, -limite, limite);
+        rotationX = Mathf.Clamp(rotationX, -limite, limite);
+
+        if (controleHorizontalInverse)
+        {
+            rotationY *= -1;
+        }
+        if (controleVerticalInverse)
+        {
+            rotationX *= -1;
+        }
+
+        Vector3 rotationSouris = new Vector3(rotationX, rotationY, 0) * vitesseRotation;
+        Vector3 rotationActuelle = transform.eulerAngles;
+
+        return Quaternion.Euler(rotationActuelle + rotationSouris);
+    }
+
+    Vector3 GestionTerrain()
+    {
+        float hauteurTerrain = Terrain.activeTerrain.transform.position.y + Terrain.activeTerrain.SampleHeight(transform.position) + distanceDuSol;
+
+        return new Vector3(transform.position.x, Mathf.Max(transform.position.y, hauteurTerrain),transform.position.z);
     }
 }
